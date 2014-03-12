b3a7cb610d864d5f0356484b802e4921621aa821
balicekt <balicekt@gmail.com>
2014-03-12 23:04:43 +0100

    update client

--- client/client.js
+++ client/client.js
@@ -3,31 +3,28 @@
 // prvni me JS na clientu
 var traineeApp = {};
 
-traineeApp.Core = (function() {
-  function Core() {
-        this.buttonEl = $('#call-backend');
-        this.responseEl = $('#response-wrapper');
-        this.io = io.connect();
-        // vygenerovani nicku tzn kazdy instance=tab ma sve idcko
-        this.nick = 'user' + Math.floor((Math.random()*1000)+1).toString(); 
-      };
-    
-  Core.prototype.initListeners = function(){
-    // _this mi drzi scope traineeApp.Core
-    var _this = this;
-    // po clicknuti na button posilam request na backend
-    this.buttonEl.click( function(){
-      _this.io.emit('first_request', {mail: _this.nick});
-    });
-    // hlidani odpovedi ze serveru a zmena html
-    this.io.on('first_response', function(data) {
-      _this.responseEl.append( data.div );
-    });
-  };
-  return Core;
-})();
+traineeApp.Core = function() {
+  this.buttonEl = $('#call-backend');
+  this.responseEl = $('#response-wrapper');
+  this.io = io.connect();
+  // vygenerovani nicku tzn kazdy instance=tab ma sve idcko
+  this.nick = 'user' + Math.floor((Math.random()*1000)+1).toString();
+};
+
+traineeApp.Core.prototype.initListeners = function(){
+  // _this mi drzi scope traineeApp.Core
+  var _this = this;
+  // po clicknuti na button posilam request na backend
+  this.buttonEl.click( function(){
+    _this.io.emit('first_request', {mail: _this.nick});
+  });
+  // hlidani odpovedi ze serveru a zmena html
+  this.io.on('first_response', function(data) {
+    divEl = $('<div class="' + data['class'] + '">' + data['userId'] + '</div>')
+    _this.responseEl.append( divEl );
+  });
+};
 
 var functionCallAbleFromEverywhere = function () {
   console.log( 'yep' );
 }
-
