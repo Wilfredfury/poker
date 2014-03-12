b261e30a4628a9b2af4519e001fdb53ca31ba347
balicekt <balicekt@gmail.com>
2014-03-13 00:03:21 +0100

    update server

--- server/data.js
+++ server/data.js
@@ -1,3 +1,50 @@
 /**
  * Created by balicekt on 12/03/14.
  */
+
+exports.users = function (){
+  var users = [
+    {
+      name: "Tomas Balicek",
+      team: "masterA",
+      email: "tomas.balicek@socialbakers.com",
+      role: "developer"
+    },
+    {
+      name: "Lukas Cerny",
+      team: "masterA",
+      email: "lukas.cerny@socialbakers.com",
+      role: "developer"
+    },
+    {
+      name: "Tomas Roch",
+      team: "masterA",
+      email: "tomas.rochk@socialbakers.com",
+      role: "developer"
+    }
+  ];
+
+  return users;
+};
+
+
+exports.userStories = function() {
+  var userStories = [
+    {
+      name: 'user story 1',
+      team: 'masterA'
+    },
+    {
+      name: 'user story 2',
+      team: 'masterA'
+    },
+    {
+      name: 'user story 3',
+      team: 'masterA'
+    }
+  ];
+
+  return userStories;
+}
+
+
