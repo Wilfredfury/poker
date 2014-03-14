ed4cc69c8baad826e8ad7803a755a28e52958e79
Lukas Cerny <luc.cerny@gmail.com>
2014-03-14 10:47:27 +0100

    [us32277] login user localStorage

--- client/js/client.js
+++ client/js/client.js
@@ -17,16 +17,25 @@ traineeApp.Core.prototype.initListeners = function(){
   // _this mi drzi scope traineeApp.Core
   var _this = this;
   var email = "";
+  var loginID = 'mail';
+
+   if(localStorage.getItem(loginID) === null){
+       _this.formEl[0].hidden = false;
+   }else{
+       email = localStorage.getItem(loginID);
+       _this.io.emit('login-request', {mail: email});
+   }
   // po clicknuti na button posilam request na backend
   this.formEl.submit( function(event){
-    event.preventDefault();
-    email = _this.emailEl.val();
+   event.preventDefault();
+   email = _this.emailEl.val();
     _this.io.emit('login-request', {mail: email});
   });
   // hlidani odpovedi ze serveru a zmena html
   this.io.on('login-response', function(data) {
     if(data.success){
       _this.user = new user(data.data);
+       localStorage.setItem(loginID,_this.user.email);
       alert(JSON.stringify(_this.user));
       _this.formEl[0].hidden = true;
       _this.contentEl.append("<p>logged in as "+_this.user.name+"</p><br><p> please wait for vote to start...</p>");
