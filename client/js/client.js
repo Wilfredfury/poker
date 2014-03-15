71c9756d5eaa20a0eca284de120c899ade2363c9
Tomáš Roch <tomas.roch@socialbakers.com>
2014-03-15 13:44:21 +0100

    [us32277] novy model, rozdeleni initu, flashMsg

--- client/js/client.js
+++ client/js/client.js
@@ -9,28 +9,37 @@ traineeApp.Core = function() {
   this.contentEl = $('#content');
   this.io = io.connect();
   this.user = {};
+
   // vygenerovani nicku tzn kazdy instance=tab ma sve idcko
   //this.nick = 'user' + Math.floor((Math.random()*1000)+1).toString();
 };
 
-traineeApp.Core.prototype.initListeners = function(){
-  // _this mi drzi scope traineeApp.Core
-  var _this = this;
-  var email = "";
-  var loginID = 'mail';
+traineeApp.Core.prototype.init = function() {
+	  // _this mi drzi scope traineeApp.Core	
+	  //	  var _this = this;
+	var loginID = 'mail';
+	  this.initListeners(loginID);
+	  this.sendLogin(loginID);
+};
 
-   if(localStorage.getItem(loginID) === null){
-       this.formEl[0].hidden = false;
-       this.formEl.submit( function(event){
-           event.preventDefault();
-           email = _this.emailEl.val();
-           _this.io.emit('login-request', {mail: email});
-       });
-   }else{
-       email = localStorage.getItem(loginID);
-       this.io.emit('login-request', {mail: email});
-   }
+traineeApp.Core.prototype.sendLogin = function(loginID) {
+	var email = "";
+	var _this = this;
+	if(localStorage.getItem(loginID) === null){
+		this.formEl[0].hidden = false;
+		this.formEl.submit( function(event){
+			event.preventDefault();
+			email = _this.emailEl.val();
+			_this.io.emit('login-request', {mail: email});
+		});
+	}else{
+		email = localStorage.getItem(loginID);
+		this.io.emit('login-request', {mail: email});
+	}
+};
 
+traineeApp.Core.prototype.initListeners = function(loginID){
+	var _this = this;
   // hlidani odpovedi ze serveru a zmena html
   this.io.on('login-response', function(data) {
     if(data.success){
@@ -40,12 +49,10 @@ traineeApp.Core.prototype.initListeners = function(){
       _this.formEl[0].hidden = true;
       _this.contentEl.append("<p>logged in as "+_this.user.name+" in team "+_this.user.team+"</p><br><p> please wait for vote to start...</p>");
     }
-    else
-      alert("user not found");
+    else{
+    	var viewInstance = new view();
+//      alert("user not found!");
+    	viewInstance.flashMsg("flashMsg", "user not found!", 'error', 10000);
+    }
   });
 };
-
-var functionCallAbleFromEverywhere = function () {
-  console.log( 'yep' );
-}
-
--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 // uklada info o "prihlasenych" uzivatelych (otevrene sockety).
-exports.usersList = {}
-
-
+exports.usersList = {};
 
 // pripojeni do objektu "exports" zajisti, ze budou objekty videt
 // z mista, kde se cely soubor requiruje
 exports.addUserList = function( userId ) {
-	exports.usersList[userId] = model.getRole(userId);
-}
-
+	exports.usersList[userId] = modelInstance.getRole(userId);
+};
 
 exports.getUserList = function(){
   return exports.usersList;
-}
+};
