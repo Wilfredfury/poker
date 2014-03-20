1e0ab38ff69dca3b1d1e3de549bc9c318f766d69
Tomáš Roch <tomas.roch@socialbakers.com>
2014-03-20 22:49:21 +0100

    [us32280] procisteni, styl, komentare

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
-// uklada info o "prihlasenych" uzivatelych (otevrene sockety).
-exports.usersList = {};
-//uklada info o aktivnich US pro hlasovani.
-exports.USList = {};
-// pripojeni do objektu "exports" zajisti, ze budou objekty videt
-// z mista, kde se cely soubor requiruje
-exports.addUserList = function( userId ) {
+exports.usersList = {}; // uklada info o "prihlasenych" uzivatelych (otevrene sockety).
+exports.USList = {}; //uklada info o aktivnich US pro hlasovani.
+
+/**
+ * vlozeni noveho uzivatele do seznamu prihlasenych uzivatelu
+ * @param userid - mail vkladaneho uzivatele
+ */
+exports.addUserList = function(userId){
 	exports.usersList[userId] = modelInstance.getRole(userId);
 };
 
+/**
+ * ziskani prihlasenych uzivatelu
+ * @returns object - list uzivatelu
+ */
 exports.getUserList = function(){
     return exports.usersList;
 };
 
-exports.addUSList = function( team, usid ) {
+/**
+ * vlozeni nove user story hlasovani do seznamu aktivnich hlasovani
+ * @param team - tym vkladane user story
+ * @param usid - ID vkladane user story
+ */
+exports.addUSList = function(team, usid){
     exports.USList[team] = usid;
 };
 
+/**
+ * ziskani aktivnich hlasovani user stories 
+ * @returns object - list user stories hlasovani
+ */
 exports.getUSList = function(){
     return exports.USList;
 };
